f1352050daca5d820f1b16058d2514119f3da53f
Derek-Qi <qiych.16@gmail.com>
2018-02-10 22:04:04 +0800

    merge parallel into sequential

--- dateUtils.py
+++ dateUtils.py
@@ -2,11 +2,10 @@ import datetime
 
 def dateRange(sdate, edate, step=datetime.timedelta(days=1), inclusive=True, weekdays=True, *args, **kwargs):
     curDate = sdate
+    if inclusive: edate += step
     while curDate < edate:
         if weekdays and curDate.weekday() > 4:
             curDate += step
             continue
         yield curDate
         curDate += step
-    if inclusive and curDate == edate:
-        yield curDate
--- getTickData.py
+++ getTickData.py
@@ -1,19 +1,21 @@
 import tushare as ts
 import datetime
-from itertools import product
 import pdb, logging
 import argparse
+from multiprocessing import Pool
+from itertools import product
 logging.basicConfig(level=logging.INFO)
 
 from dateUtils import dateRange
 
-__all_sources__ = ('sn', 'tt', 'nt')
+_all_sources = ('nt', 'sn', 'tt')
+_datadir = './data/tmp'
 
 def _get_tick_data(ticker, date):
     if not isinstance(date, str): date = str(date)
     logging.info('%s@%s' % (ticker, date))
-    all_sources = list(__all_sources__)
     df = None
+    all_sources = list(_all_sources)
     while all_sources:
         try:
             src = all_sources.pop(0)
@@ -22,11 +24,13 @@ def _get_tick_data(ticker, date):
         except OSError:
             pass
     if df is None or df.shape[0] <= 3: raise IOError('No data from all sources for ticker %s at date %s' % (ticker, date))
+    
     df.sort_values('time', inplace=True)
     df.reset_index(inplace=True, drop=True)
     df['date'] = date
     df['ticker'] = ticker
-    df.to_hdf('data/hist_ticks.h5', key='%s/%s' % (date, ticker), mode='a')
+    # df.to_hdf('./data/hist_ticks.h5', key='%s/%s' % (date, ticker), mode='a')
+    df.to_csv(_datadir + '/%s_%s.csv.gz' % (date, ticker), compression='gzip', index=False)
     logging.info('%d' % df.shape[0])
 
 def get_tick_data_serial_batch(allTickers, sdate=None, edate=datetime.date.today()):
@@ -39,20 +43,33 @@ def get_tick_data_serial_batch(allTickers, sdate=None, edate=datetime.date.today
             nowStr = str(datetime.datetime.now())
             logging.exception(nowStr)
 
-def get_tick_data_para_batch(allTickers, sdate, edate):
-    pass
+def get_tick_data_para_batch(allTickers, sdate=None, edate=datetime.date.today()):
+    if not sdate: sdate = edate
+    allDates = dateRange(sdate, edate)
+    pool = Pool()
+    for ticker, date in product(allTickers, allDates):
+        pool.apply_async(_get_tick_data, (ticker, date))
+    pool.close()
+    pool.join()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--start', '-s', type=str)
+    parser.add_argument('--start', '-s', type=str, default=None)
+    parser.add_argument('--mode', '-m', type=str, default='S')
     args = parser.parse_args()
 
     # allTickers = ts.get_hs300s()
     allTickers = ts.get_sz50s()
     allTickers = allTickers.code.tolist()
+    # Decide starting date
     if args.start:
         sdate = datetime.datetime.strptime(args.start, '%Y%m%d').date()
     else:
-        sdate = datetime.date(2017, 1, 1)
+        sdate = None
     edate = datetime.date.today()
-    get_tick_data_serial_batch(allTickers, sdate, edate)
+    logging.info('Getting tick data from date %s to date %s' % (sdate, edate))
+    # Decide mode
+    if args.mode.upper() == 'P':
+        get_tick_data_para_batch(allTickers, sdate, edate)
+    else:
+        get_tick_data_serial_batch(allTickers, sdate, edate)
